23e4817b3c8e02d9fd834c41e0dcc646536398cd
OMIDK577 <92735123+OMIDK577@users.noreply.github.com>
2024-01-06 18:28:28 +0430

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = 6438579380:AAHOQaMWqenURjwlG8v7gy9a3f1BoarbNLE
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
